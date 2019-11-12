af03f55933990243498e31aef60947d0bdd8853c
hasehiro25 <hase1969.hiro@gmail.com>
2019-11-12 22:26:39 +0900

    added to lock file when accessed

--- record_manager.rb
+++ record_manager.rb
@@ -19,27 +19,18 @@ class RecordManager
     fetch_data.find { |hash| hash["id"].to_i == id }
   end
 
-  def new_id
-    return 1 if !File.exist?(path) || fetch_data.empty?
-    max_id + 1
+  def new_id(data)
+    return 1 if !File.exist?(path) || data.empty?
+    max_id(data) + 1
   end
 
   def save(**args)
-    args[:id] = new_id
-    data = fetch_data
-    data << args
-    output_to_file(data)
+    output_to_file_with_id(args)
     args
   end
 
   def update(**args)
-    data = fetch_data
-    record = data.find { |val| val["id"] == args[:id].to_i }
-    args.delete(:id)
-    args.each do |key, val|
-      record[key.to_s] = val
-    end
-    output_to_file(data)
+    update_file(args)
   end
 
   def delete(id)
@@ -50,14 +41,34 @@ class RecordManager
   private
     attr_accessor :path
 
-    def output_to_file(data)
-      File.open(path, "w") do |file|
+    def output_to_file_with_id(args)
+      File.open(path, "r+") do |file|
         file.flock(File::LOCK_EX)
+        data = JSON.load(file)
+        args[:id] = new_id(data)
+        data << args
+        file.seek(0)
         JSON.dump(data, file)
       end
     end
 
-    def max_id
-      fetch_data.max_by { |val| val["id"].to_i }["id"].to_i
+    def update_file(args)
+      File.open(path, "r+") do |file|
+        file.flock(File::LOCK_EX)
+        data = JSON.load(file)
+        record = data.find { |val| val["id"] == args[:id].to_i }
+        args.delete(:id)
+        file.seek(0)
+        args.each do |key, val|
+          record[key.to_s] = val
+        end
+        JSON.dump(data, file)
+      end
+    end
+
+    
+
+    def max_id(data)
+      data.max_by { |val| val["id"].to_i }["id"].to_i
     end
 end
